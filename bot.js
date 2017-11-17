c89b0de2a01ed0bf570e40bd8a342290beb1d08d
netCrafter00 <33757824+netCrafter00@users.noreply.github.com>
2017-11-17 17:20:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);MzgxMTA4MDQ5MDc2NjgyNzUy.DPCW_w.Rb3gKpjPD5mbikB6r0ppw07dh5I
+client.login(process.env.BOT_TOKEN);
